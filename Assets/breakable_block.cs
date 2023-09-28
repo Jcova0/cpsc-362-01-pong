41c0c29188ae38ed0d240984191a1d1c4c7563a2
Dangkyle <124310537+Dangkyle@users.noreply.github.com>
2023-09-28 13:35:57 -0700

    changes to breakable

--- Assets/breakable_block.cs
+++ Assets/breakable_block.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class breakable_block : MonoBehaviour
+public class breakable : MonoBehaviour
 {
     // block gets destroyed when colliding with ball
     private void OnCollisionEnter2D(Collision 2D collide) {
 
-        Destroy (collide.gameObject);
+        if (OnCollisionEnter2D.gameObject.CompareTag ("breakable_block")) {
+
+            Destroy (breakable_block);
+
+        }
+
+            
     }
 
 }
