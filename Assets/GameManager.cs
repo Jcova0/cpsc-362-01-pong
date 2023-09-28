13252d30e92f48131f064f976728bce1ba5b6bf0
Marck-V <42965997+Marck-V@users.noreply.github.com>
2023-09-28 13:41:48 -0700

    Reset button now resets player positions as well

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,16 +8,9 @@ public class GameManager : MonoBehaviour
     public static int PlayerScore1 = 0;
 
     public static int PlayerScore2 = 0;
-        // Q: How do I get the player's position?
-        // A:
-        // 1. Create a public static variable to store the player's position
-        // 2. In the Start() method, find the player and store its position in the variable
-        // 3. In the Update() method, use the variable to move the player
-        // 4. In the Unity editor, drag the player object into the GameManager's Player1Pos field
-        
-        
-    public static Vector2 Player1Pos = new Vector2(-22.5f, 0);
-    public static Vector2 Player2Pos = new Vector2(22.5f, 0);
+
+    public static Vector2 Player1Pos;
+    public static Vector2 Player2Pos;
     public GUISkin layout;
     GameObject _theBall;
     
@@ -35,7 +28,7 @@ public class GameManager : MonoBehaviour
     {
         Player1Pos = GameObject.FindGameObjectWithTag("Player1").transform.position;
         Player2Pos = GameObject.FindGameObjectWithTag("Player2").transform.position;
-        print(Player1Pos);
+      
     }
     public static void Score(String goalID)
     {
@@ -59,6 +52,8 @@ public class GameManager : MonoBehaviour
         {
             PlayerScore1 = 0;
             PlayerScore2 = 0;
+            Player1Pos = GameObject.FindGameObjectWithTag("Player1").transform.position = new Vector2(-7.5f, 0);
+            Player2Pos = GameObject.FindGameObjectWithTag("Player2").transform.position = new Vector2(7.5f, 0);
             _theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
             
         }
