b6f692c895b08e01edbfa6952a677149918b317d
Jonathan Covarrubias <jcova012@gmail.com>
2023-09-28 17:09:07 -0700

    Change start key to spacebar

--- Assets/Pong Assets/BallMovement.cs
+++ Assets/Pong Assets/BallMovement.cs
@@ -17,7 +17,7 @@ public class BallMovement : MonoBehaviour
     void Update()
     {
         int rand = Random.Range(0, 1);
-        if (!isMoving && Input.anyKeyDown) {
+        if (!isMoving && Input.GetKeyDown(KeyCode.Space)) {
             if (rand == 0) {
                 this.GetComponent<Rigidbody2D>().velocity = new Vector2(-8f, 0f);
             } else {
